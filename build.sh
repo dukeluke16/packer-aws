9607b93c57ffc1be994d813aa1a007487704e977
Luke Thompson <luke.thompson@concur.com>
2018-08-22 21:05:19 -0700

    use squash for builds

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ REGISTRY=dukeluke16
 IMAGE_NAME=packer-aws
 TAG=$1
 
-docker build -t ${REGISTRY}/${IMAGE_NAME}:${TAG} -t ${REGISTRY}/${IMAGE_NAME}:latest .
+docker build --squash -t ${REGISTRY}/${IMAGE_NAME}:${TAG} -t ${REGISTRY}/${IMAGE_NAME}:latest .
 docker push ${REGISTRY}/${IMAGE_NAME}
